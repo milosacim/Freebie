92d1e6d80aec1934fff09c2eaaf597cc15cb6dbe
unknown <milosacimo@gmail.com>
2021-07-11 23:41:29 +0200

    added questions, download and footer

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active_q");
+    var content = this.nextElementSibling;
+    if (content.style.display === "none"){
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  });
+}
