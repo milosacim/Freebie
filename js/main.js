1123534e8339c882257e87dea47b0ae04f65d780
unknown <milosacimo@gmail.com>
2021-07-13 22:30:12 +0200

    responsive navbar and other changes

--- js/main.js
+++ js/main.js
@@ -13,3 +13,12 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
