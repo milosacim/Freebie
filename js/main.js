d63fa442f4c03ef5df1e66d4bc83ae498b60c894
unknown <milosacimo@gmail.com>
2021-07-15 13:08:23 +0200

    some changes

--- js/main.js
+++ js/main.js
@@ -2,16 +2,19 @@ var questions = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < questions.length; i++) {
-  questions[i].addEventListener("click", function() {
-    this.classList.toggle("active_q");
-    var content = this.nextElementSibling;
-    if (content.style.display === "none"){
+  questions[i].addEventListener("click", collapse)
+}
+
+function collapse(){
+  for (i = 0; i < questions.length; i++) {
+    questions[i].classList.toggle("active_q");
+    var content = questions[i].nextElementSibling;
+    if(content.style.display === "none"){
       content.style.display = "block";
     } else {
       content.style.display = "none";
     }
-
-  });
+  }
 }
 
 const hamburger = document.querySelector(".hamburger");
